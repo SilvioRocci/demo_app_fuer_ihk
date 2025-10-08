fbca2ee016c9bb67ea1f81bc1a0dfd648846862c
Silvio Rocci <Silvio.rocci@atos.net>
2025-10-08 15:33:41 +0200

    update 31

--- urlaub_ihk/main.tf
+++ urlaub_ihk/main.tf
@@ -3,7 +3,8 @@ terraform {
     bucket = "rocci-ihk-terraformstate"
     key = "terraform.tfstate"
     region = "eu-central-1"    
-  }  
+  } 
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
