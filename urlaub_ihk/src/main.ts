352ec22062aaa5e3bec831822086b3321d4a5913
Silvio Rocci <Silvio.rocci@atos.net>
2025-10-08 20:02:23 +0200

    Update 43

--- urlaub_ihk/src/main.ts
+++ urlaub_ihk/src/main.ts
@@ -10,7 +10,7 @@ form.addEventListener('submit', async (event) => {
 
   const name = (document.getElementById('name') as HTMLInputElement).value;
   const start = (document.getElementById('start') as HTMLInputElement).value;
-  const end = (document.getElementById('ende') as HTMLInputElement).value;
+  const end = (document.getElementById('end') as HTMLInputElement).value;
   const grund = (document.getElementById('grund') as HTMLTextAreaElement).value;
   const neuerUrlaub = { name, start, end, grund };
 
