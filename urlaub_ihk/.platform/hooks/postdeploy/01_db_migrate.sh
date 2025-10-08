23f01fc4ef8dadbd22eecfca3271f2803666ad37
Silvio Rocci <Silvio.rocci@atos.net>
2025-10-08 21:43:46 +0200

    update 51

--- urlaub_ihk/.platform/hooks/postdeploy/01_db_migrate.sh
+++ urlaub_ihk/.platform/hooks/postdeploy/01_db_migrate.sh
@@ -4,4 +4,4 @@ echo "Running DB migration..."
 mysql -h "$DB_HOST" \
       -u "$DB_USER" \
       -p"$DB_PASSWORD" \
-      "$DB_NAME" < /var/app/current/urlaub_ihk/schema.sql
+      "$DB_NAME" < /var/app/current/dist/schema.sql
