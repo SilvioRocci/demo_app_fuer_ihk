748174e66d28b1664d25b4b7c5740d5a5198cc8c
Silvio Rocci <Silvio.rocci@atos.net>
2025-10-09 15:03:10 +0200

    Update 97

--- urlaub_ihk/server.js
+++ urlaub_ihk/server.js
@@ -61,16 +61,16 @@ app.get('/api/urlaubsantraege', async (req, res) => {
 
 app.post('/api/urlaubsantraege', async (req, res) => {
   try {
-    const { name, start, ende, grund } = req.body;
+    const { name, start, end, grund } = req.body;
     console.log("Request Body:", req.body);
     console.log("start:", start, "normalized:", normalizeDate(start));
-    console.log("ende:", ende, "normalized:", normalizeDate(ende));
+    console.log("ende:", end, "normalized:", normalizeDate(end));
 
 
     const conn = await getConnection();
     const [result] = await conn.execute(
       "INSERT INTO urlaubsantraege (name, start, ende, grund) VALUES (?, ?, ?, ?)",
-      [sanitize(name), normalizeDate(start), normalizeDate(ende), sanitize(grund)]
+      [sanitize(name), normalizeDate(start), normalizeDate(end), sanitize(grund)]
     );
     await conn.end();
 
