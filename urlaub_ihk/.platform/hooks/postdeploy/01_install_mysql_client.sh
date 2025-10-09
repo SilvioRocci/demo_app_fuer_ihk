2216cfade5b9821e60478c2832cc7f99e45f7747
Silvio Rocci <Silvio.rocci@atos.net>
2025-10-09 09:22:04 +0200

    Update 76

--- urlaub_ihk/.platform/hooks/postdeploy/01_install_mysql_client.sh
+++ urlaub_ihk/.platform/hooks/postdeploy/01_install_mysql_client.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 set -e
 
-# MySQL Client installieren (je nach AMI unterschiedlich: dnf oder yum)
 if command -v dnf >/dev/null 2>&1; then
   sudo dnf install -y mariadb105
 else
   sudo yum install -y mariadb
 fi
 
-echo "▶️ Running DB migration..."
-
 mysql -h "$DB_HOST" \
       -u "$DB_USER" \
       -p"$DB_PASSWORD" \
       "$DB_NAME" < /var/app/current/schema.sql
 
-echo "✅ Schema migration done."
